2f05b89dd825abadee6431aeb8f0787368bc2ec9
Wayne.You <lokiwuxi@gmail.com>
2016-11-18 14:38:42 +0800

    draw and paint

---
+++ app/src/main/java/io/github/uv_lab/draganddraw/Box.java
@@ -0,0 +1,30 @@
+package io.github.uv_lab.draganddraw;
+
+import android.graphics.PointF;
+
+/**
+ * Created by youwei on 2016/11/18.
+ */
+
+public class Box {
+    private PointF mOrigin;
+    private PointF mCurrent;
+
+    public Box(PointF origin) {
+        mOrigin = origin;
+        mCurrent = origin;
+    }
+
+    public PointF getCurrent() {
+        return mCurrent;
+    }
+
+    public void setCurrent(PointF current) {
+        mCurrent = current;
+    }
+
+    public PointF getOrigin() {
+        return mOrigin;
+    }
+
+}
